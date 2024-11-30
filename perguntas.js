8697da7d350a8fb703f7308897f1ed86e13d465d
jorgemariano000 <00001098926006sp@al.educacao.sp.gov.br>
2024-11-29 23:16:06 -0300

    AluraQuest

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'País',
+    'Qual a capital da espanha?',
+    'Madri'
+)
+
+criaCartao(
+    'futebol',
+    'Quantas copas o brasil tem?',
+    '5'
+)
+
+criaCartao(
+    'futebol',
+    'melhor time do brasil?',
+    'Sport Club Corinthians Paulista'
+)
+
+criaCartao(
+    'futebol',
+    'o palmeiras tem mundial?',
+    'Não'
+)
